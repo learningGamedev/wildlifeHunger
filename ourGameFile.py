8a9ad376214d60eb1dd73aeea0696359b3df0593
Antvarf <antvarf@gmail.com>
2017-06-27 21:41:35 +0300

    Необходимо допилить

--- ourGameFile.py
+++ ourGameFile.py
@@ -1,41 +1,40 @@
 import random
 class Positional:
-    def __init__(self,x,y,size):
-        self.x = x
-        self.y = y
+    def __init__(self,cords,size):
+        self.cords = cords
         self.age = 0
         self.size = size
 class Plant(Positional):
-    def __init__(self, x, y, size):
-        super(Plant, self).__init__(x, y, size)
+    def __init__(self, cords, size):
+        super(Plant, self).__init__(cords, size)
 class Bush(Plant):
-    def __init__(self, x, y):
-        super(Bush, self).__init__(x,y,1.0)
+    def __init__(self, cords):
+        super(Bush, self).__init__(cords,1.0)
 class Tree(Plant):
-    def __init__(self,x,y):
-        super(Tree, self).__init__(x,y,0.5)
+    def __init__(self,cords):
+        super(Tree, self).__init__(cords,0.5)
 class Animals(Positional):
-    def __init__(self,x,y,size):
-        super(Animals, self).__init__(x,y,size)
+    def __init__(self,cords,size):
+        super(Animals, self).__init__(cords,size)
         self.speed = 0
         self.hunger = 10
         self.visionAngle = 0
         self.visionRange = 0
 class Wolf(Animals):
-    def __init__(self,x,y):
-        super(Wolf, self).__init__(x,y,0.3)
+    def __init__(self,cords):
+        super(Wolf, self).__init__(cords,0.3)
         self.visionAngle = 360
         self.visionRange = 30
 class Horse(Animals):
-    def __init__(self,x,y):
-        super(Horse, self).__init__(x,y,0.2)
+    def __init__(self,cords):
+        super(Horse, self).__init__(cords,0.2)
         self.visionAngle = 120
         self.visionRange = 20
 class Controller:
     def __init__(this, width, height):
         this.width = width
         this.height = height
-        this.plantList = [Tree(-1,-1)]
+        this.plantList = [Tree('null//null')]
         this.animalList = []
     def createPlants(this, n, opt = '0'):
         tr = 0
@@ -48,34 +47,35 @@ class Controller:
                 while this.spawnMatch == True:
                     this.xCord = random.randrange(0,this.width,1)
                     this.yCord = random.randrange(0, this.height, 1)
+                    this.ctrlString = '{0}'.format(this.xCord).zfill(3) +'//' + '{0}'.format(this.yCord).zfill(3)
                     this.m = len(this.plantList)
                     this.c = 0
                     for a in this.plantList:
-                        if (this.xCord != a.x)&(this.yCord != a.y):
+                        if (this.ctrlString != a.cords):
                             this.c += 1
                     if this.m == this.c:
                         this.spawnMatch = False
-                this.plantList.append(Tree(this.xCord,this.yCord))
+                this.plantList.append(Tree(this.ctrlString))
                 tr += 1
             else:
                 while this.spawnMatch == True:
                     this.xCord = random.randrange(0,this.width,1)
                     this.yCord = random.randrange(0, this.height, 1)
+                    this.ctrlString = '{0}'.format(this.xCord).zfill(3) + '//' + '{0}'.format(this.yCord).zfill(3)
                     this.m = len(this.plantList)
                     this.c = 0
                     for a in this.plantList:
-                        if (this.xCord != a.x)&(this.yCord != a.y):
+                        if (this.ctrlString != a.cords):
                             this.c += 1
                     if this.m == this.c:
                         this.spawnMatch = False
-                this.plantList.append(Bush(this.xCord,this.yCord))
+                this.plantList.append(Bush(this.ctrlString))
                 bu += 1
         this.plantList.__delitem__(0)
         if (this.opt == 'showList'):
             for i in this.plantList:
-                print("xCord: {0}   Y: {1}   -   {2}".format(i.x,i.y,i.__class__.__name__))
+                print("X: {0}   Y: {1}   -   {2}".format(i.cords[:3],i.cords[-3:],i.__class__.__name__))
         print("Added {0} trees and {1} bushes".format(tr,bu))
-i = Controller(30,30)
+i = Controller(45,45)
 #Необязательный параметр showList - показать список созданных объектов
-i.createPlants(30, 'showList')      #При попытке добавления 30 или меньше растений код выполняется исправно,
-                                    #иначе - бесконечное выполнение
+i.createPlants(76, 'showList')
